5937d16e2db0efbf059104a2835fa36d402f47fb
msaric1 <msaric1@mathos.hr>
2017-10-06 14:58:45 +0200

    added model.js

---
+++ model.js
@@ -0,0 +1,7 @@
+"use strict;"
+var planets = [];
+var ships = [];
+var people = [];
+var data = { people, planets, ships };
+var relatedDataUrl = { people, planets, ships };
+const numberOfDisplayingCards = 12;
